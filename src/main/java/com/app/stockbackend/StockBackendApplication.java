e2de1ab590448af5efefa3c1064ad27a25f2aeee
mehdouch <kammahdouch@gmail.com>
2023-12-07 11:20:09 +0100

    initializing the project

---
+++ src/main/java/com/app/stockbackend/StockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.stockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBackendApplication.class, args);
+	}
+
+}
